408f6b5ee7fd02ecb6ea9db1ecd9992eeb7add66
CaptureTheFrog <capturethefrog@proton.me>
2024-09-10 13:08:06 +0100

    Add flag check macro and typedef for brightness

--- kbd_bl.h
+++ kbd_bl.h
@@ -15,7 +15,7 @@
 #define KBD_BL_REQUEST_GET_STATE_DATA			((uint16_t) 0x3)
 #define KBD_BL_REQUEST_SET_STATE_DATA			((uint16_t) 0x4)
 #define KBD_BL_REQUEST_MAX						KBD_BL_REQUEST_SET_STATE_DATA
-#define KBD_BL_REQUEST_INVALID                  UINT16_MAX
+#define KBD_BL_REQUEST_INVALID                  U16_MAX
 
 #define KBD_BL_FLAGS_NONE			0
 #define KBD_BL_FLAGS_HARDWARE_BLINK	(1<<0)
@@ -23,18 +23,23 @@
 #define KBD_BL_FLAGS_FADE_ENABLED	(1<<2)
 #define KBD_BL_FLAGS_FADE_SINE		(1<<3)
 
+#define KBD_BL_IS_FLAG_SET(status, flag)    (((status_t)status).flags & flag)
+
+typedef uint8_t brightness_t;
+#define BRIGHTNESS_T_MAX                        U8_MAX
+
 typedef struct {
 	uint8_t flags;
-    uint8_t target_brightness;
-    uint8_t low_blink_target_brightness;
+    brightness_t target_brightness;
+    brightness_t low_blink_target_brightness;
 	uint32_t delay_on;
     uint32_t delay_off;
     uint32_t fade_time;
-
 } __attribute__((packed)) status_t;
 
 typedef struct{
     status_t external_status;
+    _Bool blink_state;
 } internal_status_t;
 
 #endif
