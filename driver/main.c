a6f049f5ca9204e87b38173c43b9e9bed5024005
CaptureTheFrog <capturethefrog@proton.me>
2024-09-12 12:58:33 +0100

    Add delay_on and delay_off device attrs (is there a better way to do this with the led subsystem?)

--- driver/main.c
+++ driver/main.c
@@ -6,6 +6,8 @@
 
 #define LED_NAME        "platform::kbd_backlight"
 
+// TODO: use mutexes to protect LED access! see led_cdev.led_access mutex?
+
 static struct usb_device_id your_device_table[] = {
         { USB_DEVICE(KBD_BL_USB_VENDOR_ID, KBD_BL_USB_PRODUCT_ID) },
         { } // Terminating entry
@@ -101,6 +103,159 @@ static ssize_t flags_store(struct device *dev, struct device_attribute *attr, co
 
 static DEVICE_ATTR(flags, 0664, flags_show, flags_store);
 
+
+static ssize_t delay_on_show(struct device *dev, struct device_attribute *attr, char *buf) {
+    int retval;
+    struct led_classdev* led_cdev = dev_get_drvdata(dev);
+    struct kbdbl_led* priv_data = container_of(led_cdev, struct kbdbl_led, led_cdev);
+    struct usb_device* udev = priv_data->udev;
+
+    // needs to be DMA-safe so kmalloc it
+    status_t* status = kmalloc(sizeof(status_t), GFP_KERNEL); // Buffer to receive the flag status from the device
+    if (!status) {
+        printk(KERN_ERR "%s - failed to allocate memory for status\n", __func__);
+        return -ENOMEM;
+    }
+
+    retval = usb_control_msg(udev, usb_rcvctrlpipe(udev, 0),
+                             KBD_BL_REQUEST_GET_STATE_DATA, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_DIR_IN,
+                             0, 0,
+                             status, sizeof(status_t), 50);
+    if (retval < 0) {
+        printk(KERN_ERR "%s - failed to submit control msg, error %d\n", __func__, retval);
+        kfree(status);
+        return retval;
+    }
+
+    if (retval != sizeof(status_t)){
+        printk(KERN_ERR "%s - device returned %d bytes but we expected %lu\n", __func__, retval, sizeof(status_t));
+        kfree(status);
+        return -EINVAL;
+    }
+
+    retval = snprintf(buf, PAGE_SIZE, "%d\n", status->delay_on);
+    kfree(status);
+
+    return retval;
+}
+
+static ssize_t delay_on_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+    int retval;
+    struct led_classdev* led_cdev = dev_get_drvdata(dev);
+    struct kbdbl_led* priv_data = container_of(led_cdev, struct kbdbl_led, led_cdev);
+    struct usb_device* udev = priv_data->udev;
+    
+    // needs to be DMA-safe so kmalloc it
+    typeof(((status_t*)0)->delay_on)* value = kmalloc(sizeof(((status_t*)0)->delay_on), GFP_KERNEL);
+    if (!value) {
+        printk(KERN_ERR "%s - failed to allocate memory for value\n", __func__);
+        return -ENOMEM;
+    }
+
+    if(kstrtoul(buf, 10, (unsigned long*)value) != 0){
+        printk(KERN_ERR "%s - failed to parse unsigned long\n", __func__);
+        kfree(value);
+        return -EINVAL;
+    }
+
+    retval = usb_control_msg(udev, usb_sndctrlpipe(udev, 0),
+                             KBD_BL_REQUEST_SET_STATE_DATA, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_DIR_OUT,
+                             0, offsetof(status_t, delay_on),
+                             value, sizeof(((status_t*)0)->delay_on), 5000);
+    if (retval < 0) {
+        printk(KERN_ERR "%s - failed to submit control msg, error %d\n", __func__, retval);
+        kfree(value);
+        return retval;
+    }
+
+    if (retval != sizeof(((status_t*)0)->delay_on)){
+        printk(KERN_ERR "%s - we wrote %d bytes but expected to write %lu\n", __func__, retval, sizeof(((status_t*)0)->delay_on));
+        kfree(value);
+        return -EINVAL;
+    }
+
+    kfree(value);
+    return count;
+}
+
+static DEVICE_ATTR(delay_on, 0664, delay_on_show, delay_on_store);
+
+static ssize_t delay_off_show(struct device *dev, struct device_attribute *attr, char *buf) {
+    int retval;
+    struct led_classdev* led_cdev = dev_get_drvdata(dev);
+    struct kbdbl_led* priv_data = container_of(led_cdev, struct kbdbl_led, led_cdev);
+    struct usb_device* udev = priv_data->udev;
+
+    // needs to be DMA-safe so kmalloc it
+    status_t* status = kmalloc(sizeof(status_t), GFP_KERNEL); // Buffer to receive the flag status from the device
+    if (!status) {
+        printk(KERN_ERR "%s - failed to allocate memory for status\n", __func__);
+        return -ENOMEM;
+    }
+
+    retval = usb_control_msg(udev, usb_rcvctrlpipe(udev, 0),
+                             KBD_BL_REQUEST_GET_STATE_DATA, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_DIR_IN,
+                             0, 0,
+                             status, sizeof(status_t), 50);
+    if (retval < 0) {
+        printk(KERN_ERR "%s - failed to submit control msg, error %d\n", __func__, retval);
+        kfree(status);
+        return retval;
+    }
+
+    if (retval != sizeof(status_t)){
+        printk(KERN_ERR "%s - device returned %d bytes but we expected %lu\n", __func__, retval, sizeof(status_t));
+        kfree(status);
+        return -EINVAL;
+    }
+
+    retval = snprintf(buf, PAGE_SIZE, "%d\n", status->delay_off);
+    kfree(status);
+
+    return retval;
+}
+
+static ssize_t delay_off_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
+    int retval;
+    struct led_classdev* led_cdev = dev_get_drvdata(dev);
+    struct kbdbl_led* priv_data = container_of(led_cdev, struct kbdbl_led, led_cdev);
+    struct usb_device* udev = priv_data->udev;
+
+    // needs to be DMA-safe so kmalloc it
+    typeof(((status_t*)0)->delay_off)* value = kmalloc(sizeof(((status_t*)0)->delay_off), GFP_KERNEL);
+    if (!value) {
+        printk(KERN_ERR "%s - failed to allocate memory for value\n", __func__);
+        return -ENOMEM;
+    }
+
+    if(kstrtoul(buf, 10, (unsigned long*)value) != 0){
+        printk(KERN_ERR "%s - failed to parse unsigned long\n", __func__);
+        kfree(value);
+        return -EINVAL;
+    }
+
+    retval = usb_control_msg(udev, usb_sndctrlpipe(udev, 0),
+                             KBD_BL_REQUEST_SET_STATE_DATA, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_DIR_OUT,
+                             0, offsetof(status_t, delay_off),
+                             value, sizeof(((status_t*)0)->delay_off), 5000);
+    if (retval < 0) {
+        printk(KERN_ERR "%s - failed to submit control msg, error %d\n", __func__, retval);
+        kfree(value);
+        return retval;
+    }
+
+    if (retval != sizeof(((status_t*)0)->delay_off)){
+        printk(KERN_ERR "%s - we wrote %d bytes but expected to write %lu\n", __func__, retval, sizeof(((status_t*)0)->delay_off));
+        kfree(value);
+        return -EINVAL;
+    }
+
+    kfree(value);
+    return count;
+}
+
+static DEVICE_ATTR(delay_off, 0664, delay_off_show, delay_off_store);
+
 static void your_led_brightness_set(struct led_classdev *led_cdev,
                                     enum led_brightness brightness)
 {
@@ -189,6 +344,7 @@ struct led_classdev led_cdev_template = {
     .name           = LED_NAME,
     .brightness_set = your_led_brightness_set,
     .brightness_get = your_led_brightness_get,
+    .max_brightness = BRIGHTNESS_T_MAX,
 };
 
 static int your_module_probe(struct usb_interface *interface,
@@ -221,7 +377,21 @@ static int your_module_probe(struct usb_interface *interface,
 
     int retval = device_create_file(priv_data->led_cdev.dev, &dev_attr_flags);
     if (retval) {
-        dev_err(&interface->dev, "failed to create sysfs entry\n");
+        dev_err(&interface->dev, "failed to create flags sysfs entry\n");
+        kfree(priv_data);
+        return retval;
+    }
+
+    retval = device_create_file(priv_data->led_cdev.dev, &dev_attr_delay_on);
+    if (retval) {
+        dev_err(&interface->dev, "failed to create delay_on sysfs entry\n");
+        kfree(priv_data);
+        return retval;
+    }
+
+    retval = device_create_file(priv_data->led_cdev.dev, &dev_attr_delay_off);
+    if (retval) {
+        dev_err(&interface->dev, "failed to create delay_off sysfs entry\n");
         kfree(priv_data);
         return retval;
     }
@@ -237,6 +407,8 @@ static void your_module_disconnect(struct usb_interface *interface)
         return;
     }
     device_remove_file(&interface->dev, &dev_attr_flags);
+    device_remove_file(&interface->dev, &dev_attr_delay_on);
+    device_remove_file(&interface->dev, &dev_attr_delay_off);
     led_classdev_unregister(&priv_data->led_cdev);
 }
 
